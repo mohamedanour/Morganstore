4c2881a4d2217f98886259d0f69de28ee1ba855d
mohamed abo elnour <35846483+mohamedanour@users.noreply.github.com>
2020-08-17 17:27:18 +0200

    fixing css

---
+++ wp-content/plugins/user-registration-plugin-for-woocommerce/front/js/afreg_front.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($){
+	
+	"user strict";
+	jQuery(".color_sepctrum").spectrum({
+		color: "#000",
+		preferredFormat: "hex",
+	});
+
+});
+
+jQuery(document).ready(function(){
+	"user strict";
+	jQuery('.woocommerce-form-register').attr('enctype','multipart/form-data');
+	jQuery('.woocommerce-EditAccountForm').attr('enctype','multipart/form-data');
+	
+});
+
+
+
+
+
+
+
